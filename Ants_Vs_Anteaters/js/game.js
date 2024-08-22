f6d078e3b81132aee09fffadf741cd6b7bf19985
uptimistik <uptimistik@gmail.com>
2024-08-21 22:40:57 -0400

    up

--- Ants_Vs_Anteaters/js/game.js
+++ Ants_Vs_Anteaters/js/game.js
@@ -1,206 +1,60 @@
-// Global UI functions
-function pauseGame() {
-    if (window.engine) window.engine.pause();
-}
-
-function resumeGame() {
-    if (window.engine) window.engine.play();
-}
-
-function showLeaderboardOverlay() {
-    pauseGame();
-    document.getElementById('leaderboard-overlay').style.display = 'flex';
-    startCountdownTimer();
-}
-
-function hideLeaderboardOverlay() {
-    document.getElementById('leaderboard-overlay').style.display = 'none';
-    resumeGame();
-}
-
-function showEndgameOverlay() {
-    pauseGame();
-    document.getElementById('final-score').textContent = `Your Score: ${window.currentScore}`;
-    document.getElementById('endgame-overlay').style.display = 'flex';
-}
-
-function hideEndgameOverlay() {
-    document.getElementById('endgame-overlay').style.display = 'none';
-}
-
-(function(global) {
-    var currentScore = 0,
-        lastScore = 0,
-        telegramUser = null,
-        engine = null;
-
-    global.onEngineLoad = function() {
-        gse.ready(function(gseEngine) {
-            engine = gseEngine;
-            window.engine = engine;  // Make engine globally accessible
-            var loadingElement = document.getElementById('gse-loading');
-            var playerDelegate = {
-                onTouchPressed: function() {
-                    if (navigator.vibrate) navigator.vibrate(50);
-                },
-                onGameCenterPostScore: function(score, leaderboard) {
-                    currentScore = score;
-                    window.currentScore = currentScore;  // Make currentScore globally accessible
-                    if (telegramUser) updateFirebaseScore(telegramUser.id, telegramUser.displayName, score);
-                },
-                onGameCenterShowLeaderboard: function(leaderboard) {
-                    updateLeaderboard().then(() => {
-                        showEndgameOverlay();
-                    });
-                },
-                onLoadingBegin: function() {
-                    engine.showOverlay();
-                    loadingElement.style.visibility = 'visible';
-                },
-                onLoadingEnd: function() {
-                    loadingElement.style.visibility = 'hidden';
-                    engine.hideOverlay();
-                },
-                onGameReady: function(width, height) {
-                    if (window.Telegram && window.Telegram.WebApp) {
-                        telegramUser = window.Telegram.WebApp.initDataUnsafe.user;
-                        if (telegramUser) {
-                            let displayName = telegramUser.username;
-                            if (!displayName) {
-                                displayName = telegramUser.first_name;
-                                if (telegramUser.last_name) displayName += ' ' + telegramUser.last_name;
-                            }
-                            telegramUser.displayName = displayName;
-                            engine.postEvent('externalWriteGameAttribute', null, "game.attributes.telegramUser", {
-                                id: telegramUser.id,
-                                name: displayName
-                            });
-                        }
-                    }
-                    // Start the game immediately
-                    resumeGame();
-                },
-                onWindowResize: function() {
-                    engine.relayout();
-                }
-            };
-
-            engine.appendDelegate(playerDelegate);
-            window.addEventListener('resize', playerDelegate.onWindowResize, false);
-            engine.setRenderFrame('gse-player');
-            engine.setOptions({
-                'viewport-reference': 'window',
-                'viewport-fit': 'letterbox'
-            });
-            engine.loadOptionsFromURL();
-
-            // Event Listeners
-            document.getElementById('close-leaderboard-button').addEventListener('click', function() {
-                hideLeaderboardOverlay();
-            });
-
-            document.getElementById('view-leaderboard-button').addEventListener('click', function() {
-                updateLeaderboard().then(() => {
-                    hideEndgameOverlay();
-                    showLeaderboardOverlay();
-                });
-            });
-
-            document.getElementById('share-button').addEventListener('click', function() {
-                if (window.Telegram && window.Telegram.WebApp) {
-                    const shareText = encodeURIComponent(`I just scored ${currentScore} kills in Ant Game! Can you beat me?`);
-                    const shareUrl = `https://t.me/share/url?url=https://t.me/@Antsgame_bot&text=${shareText}`;
-                    window.Telegram.WebApp.openTelegramLink(shareUrl);
-                }
-            });
-
-            document.getElementById('replay-button').addEventListener('click', function() {
-                hideEndgameOverlay();
-                engine.postEvent('resetGame', null, null, null);
-                resumeGame();
-            });
-        });
-    };
-
-    function updateFirebaseScore(userId, userName, score) {
-        const userRef = firebase.database().ref('users/' + userId);
-        userRef.once('value').then((snapshot) => {
-            const userData = snapshot.val();
-            if (!userData || userData.score < score) {
-                userRef.set({
-                    id: userId,
-                    name: userName,
-                    score: score
-                });
-            }
-            lastScore = score;
-        });
-    }
-
-    function updateLeaderboard() {
-        return new Promise((resolve, reject) => {
-            const leaderboardRef = firebase.database().ref('users');
-            leaderboardRef.orderByChild('score').once('value', (snapshot) => {
-                const leaderboardData = [];
-                snapshot.forEach((childSnapshot) => {
-                    leaderboardData.unshift({
-                        id: childSnapshot.key,
-                        ...childSnapshot.val()
-                    });
-                });
-                updateLeaderboardWithCurrentPlayer(leaderboardData);
-                resolve();
-            }, (error) => {
-                console.error("Error fetching leaderboard data:", error);
-                reject(error);
-            });
-        });
-    }
-
-    function updateLeaderboardWithCurrentPlayer(leaderboardData) {
-        let leaderboardHtml = '<tr><th>Rank</th><th>Ant Name</th><th>Top Kills</th></tr>';
-        let currentPlayerHighScore = 0;
-        leaderboardData.forEach((user, index) => {
-            const rank = index + 1;
-            const initials = user.name.charAt(0).toUpperCase();
-            const avatarHtml = `<div class="avatar">${initials}</div>`;
-            const isCurrentPlayer = user.id === telegramUser.id;
-            const currentPlayerIcon = isCurrentPlayer ? '<span class="current-player-icon"></span>' : '';
-            if (isCurrentPlayer) currentPlayerHighScore = user.score;
-            leaderboardHtml += `<tr><td>${rank}</td><td>${avatarHtml}${user.name}${currentPlayerIcon}</td><td>${user.score}</td></tr>`;
-        });
-        document.getElementById('leaderboard').innerHTML = leaderboardHtml;
-        document.getElementById('total-ants').textContent = `Total Ants: ${leaderboardData.length}`;
-        document.getElementById('your-last-score').textContent = `Your Last Score: ${lastScore}`;
-        if (currentScore > currentPlayerHighScore) {
-            document.getElementById('new-high-score').textContent = `New High Score: ${currentScore}`;
-            document.getElementById('new-high-score').style.display = 'block';
-        } else {
-            document.getElementById('new-high-score').style.display = 'none';
-        }
-    }
-
-    function updateCountdown() {
-        const now = new Date();
-        const target = new Date("2024-07-21T00:00:00Z");
-        
-        if (now >= target) {
-            document.getElementById('leaderboard-countdown').textContent = "Leaderboard reset is in progress!";
-            return;
-        }
-        
-        const timeLeft = target - now;
-        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-        
-        document.getElementById('leaderboard-countdown').textContent = `Leaderboard resets in: ${days}d ${hours}h ${minutes}m ${seconds}s`;
-    }
-
-    function startCountdownTimer() {
-        updateCountdown();
-        setInterval(updateCountdown, 1000);
-    }
-
-})(window);
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Colony Clash - $ANT</title>
+    <link rel="stylesheet" type="text/css" href="css/gse-style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/gse-style-loading.css"/>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+</head>
+<body>
+    <div id="gse-player" class="gse-frame">
+        <div class="gse-overlay">
+            <div id="gse-text" class="gse-dialog">
+                <div>
+                    <button id="gse-text-cancel">Cancel</button>
+                    <button id="gse-text-done">Done</button>
+                    <p id="gse-text-prompt"></p>
+                </div>
+                <div>
+                    <textarea id="gse-text-input"></textarea>
+                </div>
+            </div>
+            <div id="gse-loading" style="visibility:visible;">
+                <img src="images/gse-loading.png"/>
+            </div>
+        </div>
+    </div>
+    <div id="start-overlay" class="overlay">
+        <h1>Ant Name: <span id="telegram-username"></span></h1>
+        <button id="leaderboard-button">Leaderboard</button>
+    </div>
+    <div id="leaderboard-overlay" class="overlay" style="display:none;">
+        <h2>Ant Leaderboard</h2>
+        <div id="total-ants"></div>
+        <div id="your-last-score"></div>
+        <div id="leaderboard-countdown"></div>
+        <div id="leaderboard-container">
+            <table id="leaderboard">
+                <tr><th>Rank</th><th>Ant Name</th><th>Top Kills</th></tr>
+            </table>
+        </div>
+        <button id="close-leaderboard-button">Close</button>
+    </div>
+    <div id="endgame-overlay" class="overlay" style="display:none;">
+        <h2>Game Over</h2>
+        <div id="final-score"></div>
+        <div id="new-high-score" style="display:none;"></div>
+        <button id="view-leaderboard-button">View Leaderboard</button>
+        <button id="share-button">Share</button>
+        <button id="replay-button">Play Again</button>
+    </div>
+    <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/game.js"></script>
+    <script type="text/javascript" src="js/gse/gse-export.js" async onload="onEngineLoad()"></script>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+</body>
+</html>
